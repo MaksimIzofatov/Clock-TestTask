ae4f13bdcdf4c156a0cf2aa29bbafa66881efbf9
Maxim <maxizofat@gmail.com>
2025-02-07 18:08:38 +0300

    4

---
+++ Assets/Scripts/TimeService.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using UnityEngine;
+
+
+public class TimeService : MonoBehaviour
+{
+    private const string Url = "https://yandex.com/time/sync.json";
+
+    public  void GetCurrentTimeAsync()
+    {
+        
+    }
+
+}
