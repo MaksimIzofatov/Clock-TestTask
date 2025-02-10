39d35bbae234a3d343a94ff4e7ef00eec09d06cf
MaksimIzofatov <giantsgnom@gmail.com>
2025-02-10 19:13:20 +0300

    refactoring Get time for server

--- Assets/Scripts/TimeService.cs
+++ Assets/Scripts/TimeService.cs
@@ -2,27 +2,32 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.Networking;
 
 
 public class TimeService
 {
     public async Task<DateTime> LoadTimeFromServer()
         {
-            try
-            {
-                using (HttpClient client = new HttpClient())
+                using (UnityWebRequest client = UnityWebRequest.Get(GlobalConstants.Links.TIME_URL))
                 {
-                    var response = await client.GetStringAsync(GlobalConstants.Links.TIME_URL);
+                    var send = client.SendWebRequest();
+                    while (!send.isDone)
+                    {
+                        await Task.Yield();
+                    }
+
+                    if (client.result == UnityWebRequest.Result.ConnectionError ||
+                        client.result == UnityWebRequest.Result.ProtocolError)
+                    {
+                        return DateTime.Now;
+                    }
+                    
+                    var response = client.downloadHandler.text;
                     var json = JsonUtility.FromJson<TimeTemplate>(response);
                     var offset = DateTimeOffset.FromUnixTimeMilliseconds(json.time).ToLocalTime();
 
                     return offset.DateTime;
                 }
-            }
-            catch (HttpRequestException e)
-            {
-                Debug.Log(e.Message);
-                return DateTime.Now;
-            }
         }
 }
